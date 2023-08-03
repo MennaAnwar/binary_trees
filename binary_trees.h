9bf95cea491ee3388fb0db92ea25b111a7c2b9ee
MennaAnwar <menna_m20@yahoo.com>
2023-08-03 13:01:20 -0700

    20. Level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -55,5 +55,6 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_complete(const binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
