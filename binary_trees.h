00a10e485f94dbec8ded66ed4e5f872560fcfba7
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-08-04 03:26:26 +0300

    32. AVL - Array to AVL

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
